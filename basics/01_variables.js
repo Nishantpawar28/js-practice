a0ab02ea1e99fcdaabe75d6d556171a92f4de2c5
Nishant Pawar <ypawar31111@gmail.com>
2024-10-06 09:08:05 +0000

    variables and datatypes basic

---
+++ basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 122;
+let accountEmail = "ab@gmail.com"
+var accPassword = "1234"
+accountCity = "Jaipur"
+
+//don't use var as they are function scoped
+
+accountEmail = "dsf"
+accPassword = "324fdg"
+accountCity = "fd"
+
+console.table([accountId, accountEmail, accPassword, accountCity])
---
+++ basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+/*when we ddeclare a variable and forgot to assign value then it 
+is undefined but assign null means we are assigning a empty value*/
+
+
+//type of null is object
+//type of undefined is undefined
+
+
+//primitive data type
+// number
+// bigInt
+// boolean
+// null
+// undefined
+// symbol
