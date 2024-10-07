d92371869b18725007d6e090bca063471f80caeb
Nishant Pawar <ypawar31111@gmail.com>
2024-10-07 07:35:09 +0000

    Conversion of data types

---
+++ basics/03_conversionOperation.js
@@ -0,0 +1,21 @@
+let val = "11abc"
+
+let newVal = Number(val)
+
+console.log(newVal)
+console.log(typeof(newVal))
+
+//conversion results
+// "33" => 33
+// "33ac" => NaN
+// true => 1;  false => 0
+// unddefined => NaN
+// null => 0
+
+
+//String and Boolean methods are also used for conversion
+
+//boolean
+// 1 => true;  0 => false
+// "" => false
+// "lisa" => true 
