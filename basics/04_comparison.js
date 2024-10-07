36d6fa238e8d7243279cad61b195e54f710298b8
Nishant Pawar <ypawar31111@gmail.com>
2024-10-07 08:17:51 +0000

    Dataypes and comparisons

---
+++ basics/04_comparison.js
@@ -0,0 +1,2 @@
+console.log("2" == 2)   //conversion happens so returns true
+console.log("2" === 2)  //both values should have same data type for checking so returns false
---
+++ datatypes_summary.js
@@ -0,0 +1,37 @@
+//Primitive   
+//These types are call by value i.e whenver it is copied smewhere then the value is copied 
+//and not the memory reference so changes made in the copied variable won't effect the original variable
+
+// 7 types: string, number, boolean, null, undefined, symbol, bigInt
+
+
+// Non-primitve/Reference
+
+// Arrays, Objects, Functions
+
+
+//JavaScript is a dynamically typed language, meaning that type checking happens at runtime, rather than 
+//during compilation. In dynamically typed languages, variables can store different types of data, and 
+//you don't need to explicitly declare a variable's data type
+
+
+// Typescript is Optional static typing is a compromise between static and dynamic typing.
+
+
+//Type of null is an Object
+
+//Datatypes of all non primitives are called as functions
+
+const arr = [2,43,5]
+
+const obj = {
+    name: "lisa"
+}
+
+const func = function() {
+    console.log("helo")
+}
+
+console.log(typeof arr)   //type is object
+console.log(typeof obj)     //type is object
+console.log(typeof func)    //type is function
